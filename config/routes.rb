6e4a11bb2b5662823a05caf5e264f29eb9b04395
RossLitz <codingclasses.049@gmail.com>
2015-02-04 14:47:33 -0600

    add Post to book and tested

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :books, except: [:destory, :update]
+  resources :books
+  resources :finished_books
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
